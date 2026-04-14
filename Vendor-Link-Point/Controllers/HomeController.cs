0f8050f38196ed7ea273eda2ac67469739ee6ccf
Wild Péter <wildpeter10@gmail.com>
2026-04-14 08:10:57 +0200

    Innit base files

---
+++ Vendor-Link-Point/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Vendor_Link_Point.Models;
+
+namespace Vendor_Link_Point.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Vendor-Link-Point/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Vendor_Link_Point.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
