216306cf8c149ccbff2bc5dfd39179d528d13366
Petya122 <wildpeter10@gmail.com>
2026-04-17 10:30:11 +0200

    Extended Migration with the Extended Base Models

--- Vendor-Link-Point/Migrations/20260417082810_ExtendedBaseModels.Designer.cs
+++ Vendor-Link-Point/Migrations/20260417082810_ExtendedBaseModels.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Vendor_Link_Point.Data;
 
@@ -11,9 +12,11 @@ using Vendor_Link_Point.Data;
 namespace Vendor_Link_Point.Migrations
 {
     [DbContext(typeof(VendorLinkPointContext))]
-    partial class VendorLinkPointContextModelSnapshot : ModelSnapshot
+    [Migration("20260417082810_ExtendedBaseModels")]
+    partial class ExtendedBaseModels
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
