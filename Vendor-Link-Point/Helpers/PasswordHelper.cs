0438cde4ae85dc4cc6a9b4e101c7aa91204df7b5
Petya122 <wildpeter10@gmail.com>
2026-04-17 14:28:01 +0200

    Added AccountController with Register and Login (GET, POST) Added PasswordHelper for password encoding Added StringHelper for generating random new seller ID

---
+++ Vendor-Link-Point/Helpers/PasswordHelper.cs
@@ -0,0 +1,25 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace Vendor_Link_Point.Helpers
+{
+    public static class PasswordHelper
+    {
+        public static string HashPasswordSHA256(string password)
+        {
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                // A jelszó byte-tömbbé alakítása
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(password));
+
+                // A byte-tömb visszaalakítása olvasható hexadecimális stringgé
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+                return builder.ToString();
+            }
+        }
+    }
+}
