5f83f025e6a0bf8b1cb316e3df2bfb304017048b
Petya122 <wildpeter10@gmail.com>
2026-06-01 09:56:21 +0200

    Removed warnings from AccountViewModel.cs

--- Vendor-Link-Point/ViewModels/AccountViewModel.cs
+++ Vendor-Link-Point/ViewModels/AccountViewModel.cs
@@ -7,51 +7,51 @@ namespace Vendor_Link_Point.ViewModels
         [Required(ErrorMessage = "Az e-mail cím megadása kötelező!")]
         [EmailAddress(ErrorMessage = "Érvénytelen e-mail formátum!")]
         [Display(Name = "E-mail cím")]
-        public string Email { get; set; }
+        public required string Email { get; set; }
 
         [Required(ErrorMessage = "A jelszó megadása kötelező!")]
         [DataType(DataType.Password)]
         [Display(Name = "Jelszó")]
-        public string Jelszo { get; set; }
+        public required string Jelszo { get; set; }
     }
 
     public class RegisterViewModel
     {
         [Required(ErrorMessage = "A név megadása kötelező!")]
         [Display(Name = "Teljes név")]
-        public string Nev { get; set; }
+        public required string Nev { get; set; }
 
         [Required(ErrorMessage = "Az e-mail cím megadása kötelező!")]
         [EmailAddress(ErrorMessage = "Érvénytelen e-mail formátum!")]
         [Display(Name = "E-mail cím")]
-        public string Email { get; set; }
+        public required string Email { get; set; }
 
         [Required(ErrorMessage = "A jelszó megadása kötelező!")]
         [DataType(DataType.Password)]
         [MinLength(6, ErrorMessage = "A jelszónak legalább 6 karakternek kell lennie!")]
         [Display(Name = "Jelszó")]
-        public string Jelszo { get; set; }
+        public required string Jelszo { get; set; }
 
         [Required(ErrorMessage = "A jelszó megerősítése kötelező!")]
         [DataType(DataType.Password)]
         [Compare("Jelszo", ErrorMessage = "A két jelszó nem egyezik!")]
         [Display(Name = "Jelszó megerősítése")]
-        public string JelszoUjra { get; set; }
+        public required string JelszoUjra { get; set; }
 
         [Required(ErrorMessage = "Válassz fióktípust!")]
         [Display(Name = "Fiók típusa")]
-        public string Role { get; set; } // Ide jön a "Vasarlo" vagy "Kereskedo"
+        public required string Role { get; set; } // Ide jön a "Vasarlo" vagy "Kereskedo"
 
         // --- Vásárló specifikus mezők (nullable, mert kereskedőnél üres lesz) ---
         [Display(Name = "Szállítási cím")]
-        public string? SzallitasiCim { get; set; }
+        public required string SzallitasiCim { get; set; }
 
         [Display(Name = "Telefonszám")]
-        public string? Telefonszam { get; set; }
+        public required string Telefonszam { get; set; }
 
         // --- Kereskedő specifikus mezők (nullable) ---
         [Display(Name = "Kereskedő Azonosító")]
-        public string? KereskedoId { get; set; }
+        public required string KereskedoId { get; set; }
 
         [Display(Name = "Cégnév / Bolt neve")]
         public string? Cegnev { get; set; }
