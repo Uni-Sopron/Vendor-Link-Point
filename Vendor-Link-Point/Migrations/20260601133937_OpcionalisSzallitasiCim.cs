d09a7059be44082e71735b48e289c31d25cb5dc5
Petya122 <wildpeter10@gmail.com>
2026-06-01 15:40:15 +0200

    Optional Migration

---
+++ Vendor-Link-Point/Migrations/20260601133937_OpcionalisSzallitasiCim.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Vendor_Link_Point.Migrations
+{
+    /// <inheritdoc />
+    public partial class OpcionalisSzallitasiCim : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
