d390bf7487236db8c1cf1fbae94533b04ecc1206
Petya122 <wildpeter10@gmail.com>
2026-06-01 15:24:46 +0200

    Added Kedvenc.cs

---
+++ Vendor-Link-Point/Models/Kedvenc.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Vendor_Link_Point.Models
+{
+    public class Kedvenc
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public int UserId { get; set; }
+        [ForeignKey("UserId")]
+        public virtual User Vasarlo { get; set; } = null!;
+
+        [Required]
+        public int ProductId { get; set; }
+        [ForeignKey("ProductId")]
+        public virtual Product Termek { get; set; } = null!;
+    }
+}
